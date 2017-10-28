29084187e503e9d7071e63fa58455eb29e92af53
Batuhan İşlek <islekrb@gmail.com>
2017-10-28 12:35:10 +0300

    login page template is added, links are implemented to layout, server.py updated for links

--- server.py
+++ server.py
@@ -12,6 +12,26 @@ app = Flask(__name__)
 def home_page():
     return render_template('home.html')
 
+@app.route('/store')
+def store_page():
+    return render_template('store.html')
+
+@app.route('/library')
+def library_page():
+    return render_template('library.html')
+
+@app.route('/blog')
+def blog_page():
+    return render_template('blog.html')
+
+@app.route('/profile')
+def profile_page():
+    return render_template('profile.html')
+
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
+
 
 if __name__ == '__main__':
     VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
@@ -19,4 +39,4 @@ if __name__ == '__main__':
         port, debug = int(VCAP_APP_PORT), False
     else:
         port, debug = 5000, True
-    app.run(host='0.0.0.0', port=port, debug=debug)
+    app.run(host='localhost', port=port, debug=debug)
