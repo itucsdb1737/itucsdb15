17e360e5fdbfa4d2e29c5847e720b2f567be91dc
Batuhan İşlek <islekrb@gmail.com>
2017-10-30 12:38:22 +0200

    elephantSQL configuration and db initialization is made

--- server.py
+++ server.py
@@ -1,11 +1,48 @@
 import datetime
 import os
+import json
+import re
+import psycopg2 as dbapi2
 
 from flask import Flask
 from flask import render_template
+from flask import redirect
+from flask.helpers import url_for
 
 
 app = Flask(__name__)
+
+
+def get_elephantsql_dsn(vcap_services):
+    parsed = json.loads(vcap_services)
+    uri = parsed["elephantsql"][0]["credentials"]["uri"]
+    match = re.match('postgres://(.*?):(.*?)@(.*?)(:(\d+))?/(.*)', uri)
+    user, password, host, _, port, dbname = match.groups()
+    dsn = """user='{}' password='{}' host='{}' port={}
+             dbname='{}'""".format(user, password, host, port, dbname)
+    return dsn
+
+
+
+
+@app.route('/initdb_1737')
+def initialize_database():
+    with dbapi2.connect(app.config['dsn']) as connection:
+        cursor = connection.cursor()
+
+        query = """DROP TABLE IF EXISTS USERS"""
+        cursor.execute(query)
+
+        query = """CREATE TABLE USERS(
+                 ID SERIAL NOT NULL,
+                 USERNAME VARCHAR(30),
+                 PASSWORD VARCHAR(30),
+                 PRIMARY KEY(ID)
+                 )"""
+        cursor.execute(query)
+
+        connection.commit()
+    return redirect(url_for('home_page'))
 
 
 @app.route('/')
@@ -39,4 +76,11 @@ if __name__ == '__main__':
         port, debug = int(VCAP_APP_PORT), False
     else:
         port, debug = 5000, True
+
+    VCAP_SERVICES = os.getenv('VCAP_SERVICES')
+    if VCAP_SERVICES is not None:
+        app.config['dsn'] = get_elephantsql_dsn(VCAP_SERVICES)
+    else:
+        app.config['dsn'] = """user='postgres' password='Batuhan190393'
+                               host='localhost' port=5432 dbname='postgres'"""
     app.run(host='0.0.0.0', port=port, debug=debug)
