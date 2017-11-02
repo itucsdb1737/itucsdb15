77d22444f45d46dc607e9b87a554e8ba1d1a89ab
Batuhan İşlek <islekrb@gmail.com>
2017-11-02 13:17:55 +0300

    Changed to flask_wtf instead of wtform for bluemix

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import json
 import re
 import psycopg2 as dbapi2
 
-from wtforms import Form
+from flask_wtf import Form
 from flask import json
 from functools import wraps
 from wtforms import StringField, PasswordField, SubmitField, validators, BooleanField
